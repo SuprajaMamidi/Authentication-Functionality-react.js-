3f46023fcddca7109bafa0cf22ca156e631e23c1
SuprajaMamidi <supraja82002@gmail.com>
2024-04-29 15:56:34 +0000

    uthentication functionality

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,11 @@
+// Write your JS code here
+import './index.css'
+
+const NotFound = () => (
+  <>
+    <div className="not_found">
+      <h1>Not Found</h1>
+    </div>
+  </>
+)
+export default NotFound
